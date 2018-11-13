01ae54d4cd4fb33a56c9e70181305cc251c0e56f
RajashriTarate <39621760+RajashriST@users.noreply.github.com>
2018-11-13 18:49:23 +0530

    Create antivowel.py

---
+++ antivowel.py
@@ -0,0 +1,16 @@
+def anti_vowel(text):
+  i=0
+  s=""
+  while i<len(text):     
+         if text[i] in "aeiouAEIOU":
+             #s=s+text[i]
+             pass
+         else:
+             s=s+text[i]
+         i=i+1
+  return s
+st=str(input("Enter :"))
+print("You entered : ",st)
+print()
+anti_vowel(st)
+print(s)
